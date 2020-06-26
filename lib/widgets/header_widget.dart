e4b5ac3820902d78cff1a084510884fa467cb89c
mari <mari.17ymy@gmail.com>
2020-06-25 22:11:43 -0400

    header circular

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HeaderWidget extends StatelessWidget {
+class HeaderCuadradoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300.0,
       color: Colors.greenAccent,
+    );
+  }
+}
+
+class HeaderCircularWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40.0),
+    //  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40.0)),
+        color: Colors.greenAccent,
+      )
     );
   }
 }
