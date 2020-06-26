0b2da6c350811fcc0ba991d35b344867f5cab08b
mari <mari.17ymy@gmail.com>
2020-06-25 23:26:09 -0400

    triangulo izquierdo

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -96,3 +96,38 @@ class _HeaderTrianguloPainter extends CustomPainter {
   @override
   bool shouldRepaint(_HeaderTrianguloPainter oldDelegate) => true;
 }
+
+class HeaderTrianguloIzWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderTrianguloIzPainter(),
+      )
+    );
+  }
+}
+class _HeaderTrianguloIzPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+    // propiedades
+    lapiz.color = Colors.orangeAccent;
+    lapiz.strokeWidth = 20.0;
+    lapiz.style = PaintingStyle.fill;
+
+    final path = new Path();
+    //dibujar con el lapiz y el path
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+  @override
+  bool shouldRepaint(_HeaderTrianguloIzPainter oldDelegate) => true;
+}
