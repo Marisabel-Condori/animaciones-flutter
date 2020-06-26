bba92a6e49368b283cb252da22de734332cfda37
mari <mari.17ymy@gmail.com>
2020-06-25 23:22:53 -0400

    triangulo derecho

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -58,8 +58,41 @@ class _HeaderDiagonalPainter extends CustomPainter {
 
     canvas.drawPath(path, lapiz);
   }
-
   @override
   bool shouldRepaint(_HeaderDiagonalPainter oldDelegate) => true;
+}
 
+class HeaderTrianguloWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderTrianguloPainter(),
+      )
+    );
+  }
+}
+class _HeaderTrianguloPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+    // propiedades
+    lapiz.color = Colors.orangeAccent;
+    lapiz.strokeWidth = 20.0;
+    lapiz.style = PaintingStyle.fill;
+
+    final path = new Path();
+    //dibujar con el lapiz y el path
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+  @override
+  bool shouldRepaint(_HeaderTrianguloPainter oldDelegate) => true;
 }
