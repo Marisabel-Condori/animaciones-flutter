11dc2c977e97dcf30b9aa3aa6fb5b25eadecbc69
mari <mari.17ymy@gmail.com>
2020-06-25 22:02:14 -0400

    header cuadrado

---
+++ lib/pages/header_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:custom_painter/widgets/header_widget.dart';
+
+class HeadersPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderWidget()
+    );
+  }
+}
---
+++ lib/widgets/header_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HeaderWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      color: Colors.greenAccent,
+    );
+  }
+}
