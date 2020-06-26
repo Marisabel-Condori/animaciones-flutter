bad8f0d67112a61503f73fae1409b28294451af3
mari <mari.17ymy@gmail.com>
2020-06-25 23:07:15 -0400

    header diagonal

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -25,3 +25,41 @@ class HeaderCircularWidget extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonalWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      )
+    );
+  }
+}
+class _HeaderDiagonalPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+    // propiedades
+    lapiz.color = Colors.blueGrey;
+    lapiz.strokeWidth = 20.0;
+    lapiz.style = PaintingStyle.fill;
+
+    final path = new Path();
+    //dibujar con el lapiz y el path
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(_HeaderDiagonalPainter oldDelegate) => true;
+
+}
