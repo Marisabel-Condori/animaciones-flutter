e9291cdb62fc522febf5d54095da49590f3e70ae
mari <mari.17ymy@gmail.com>
2020-06-25 23:36:45 -0400

    header pico

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -131,3 +131,39 @@ class _HeaderTrianguloIzPainter extends CustomPainter {
   @override
   bool shouldRepaint(_HeaderTrianguloIzPainter oldDelegate) => true;
 }
+//*************************************************** */
+class HeaderPicoWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderPicoPainter(),
+      )
+    );
+  }
+}
+class _HeaderPicoPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+    // propiedades
+    lapiz.color = Colors.orangeAccent;
+    lapiz.strokeWidth = 20.0;
+    lapiz.style = PaintingStyle.fill;
+
+    final path = new Path();
+    //dibujar con el lapiz y el path
+    path.lineTo(0, size.height * 0.2);
+    path.lineTo(size.width * 0.5, size.height * 0.4);
+    path.lineTo(size.width, size.height * 0.2);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+  @override
+  bool shouldRepaint(_HeaderPicoPainter oldDelegate) => true;
+}
