3d269ca38784c491e1bb1934ae451d7044152e6b
mari <mari.17ymy@gmail.com>
2020-06-25 23:47:36 -0400

    header curvo

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -167,3 +167,40 @@ class _HeaderPicoPainter extends CustomPainter {
   @override
   bool shouldRepaint(_HeaderPicoPainter oldDelegate) => true;
 }
+
+//*************************************************** */
+class HeaderCurvaWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderCurvaPainter(),
+      )
+    );
+  }
+}
+class _HeaderCurvaPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+    // propiedades
+    lapiz.color = Colors.orangeAccent;
+    lapiz.strokeWidth = 20.0;
+    lapiz.style = PaintingStyle.fill;
+
+    final path = new Path();
+    //dibujar con el lapiz y el path
+    path.lineTo(0, size.height * 0.2);
+    path.quadraticBezierTo(size.width * 0.5, size.height * 0.4, size.width, size.height * 0.2);
+    path.lineTo(size.width, 0);
+    
+
+    canvas.drawPath(path, lapiz);
+  }
+  @override
+  bool shouldRepaint(_HeaderCurvaPainter oldDelegate) => true;
+}
