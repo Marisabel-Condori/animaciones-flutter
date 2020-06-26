5ddb3acb561dc53d0e0b157a985f75f0734fdff9
mari <mari.17ymy@gmail.com>
2020-06-26 00:49:37 -0400

    animation y animationController

--- lib/pages/animaciones_page.dart
+++ lib/pages/animaciones_page.dart
@@ -18,7 +18,22 @@ class CuadradoAnimado extends StatefulWidget {
   _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = new AnimationController(vsync: this, duration: Duration(milliseconds: 4000));
+    rotacion= Tween(begin: 0.0, end: 2.0).animate(controller);
+    super.initState();
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return _Cuadrado();
