ed8d7e5df1e14a456aa883cb1cd7a6005fda7e93
mari <mari.17ymy@gmail.com>
2020-06-26 23:04:01 -0400

    CurvedAnimation

--- lib/pages/animaciones_page.dart
+++ lib/pages/animaciones_page.dart
@@ -26,10 +26,12 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProv
   @override
   void initState() {
     controller = new AnimationController(vsync: this, duration: Duration(milliseconds: 4000));
-    rotacion= Tween(begin: 0.0, end: 2 * Math.pi).animate(controller);
+    rotacion= Tween(begin: 0.0, end: 2 * Math.pi).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeInBack)
+    );
 
     controller.addListener(() {
-      print('status: ${controller.status}');
+      // print('status: ${controller.status}');
       if(controller.status == AnimationStatus.completed){
         controller.reverse();
        }//else if (controller.status == AnimationStatus.dismissed) {
@@ -54,7 +56,7 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProv
        animation: controller,
       //  child: _Cuadrado(),
       builder: (BuildContext context, Widget child) {
-        print(rotacion.value);
+        // print(rotacion.value);
         return Transform.rotate(
           angle: rotacion.value,
           child: _Cuadrado()
