d9c713f7bef749c645ec7b0f1dbd0e56198af8cb
mari <mari.17ymy@gmail.com>
2020-06-27 22:38:53 -0400

    circular progress ya dibujado

--- lib/pages/circular_progress_page.dart
+++ lib/pages/circular_progress_page.dart
@@ -1,7 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CircularProgressPage extends StatefulWidget {
-
   @override
   _CircularProgressPageState createState() => _CircularProgressPageState();
 }
@@ -14,9 +15,47 @@ class _CircularProgressPageState extends State<CircularProgressPage> {
         child: Container(
           height: 300.0,
           width: 300.0,
-          color: Colors.deepPurpleAccent,
+          child: CustomPaint(
+            painter: _MiRadialProgress(40)
+          ),
         ),
       )
     );
   }
+}
+
+class _MiRadialProgress extends CustomPainter{
+  final porcentaje;
+  _MiRadialProgress(this.porcentaje);
+  @override
+  void paint(Canvas canvas, Size size) {
+      final paint = new Paint()
+      ..strokeWidth = 5
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke;
+
+      Offset centro = Offset(size.width * 0.5, size.height/2);
+      double radio = min(size.width * 0.5, size.height*0.5);
+      canvas.drawCircle(centro, radio, paint);
+
+      //arco
+      final paintArco = new Paint()
+      ..strokeWidth = 10
+      ..color = Colors.orangeAccent
+      ..style = PaintingStyle.stroke;
+
+      double arcAngulo = 2*pi*(porcentaje /100);
+
+      canvas.drawArc(
+        Rect.fromCircle(center: centro,radius: radio),
+        -pi/2, 
+        arcAngulo, 
+        false, 
+        paintArco
+      );
+    }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+
 }
