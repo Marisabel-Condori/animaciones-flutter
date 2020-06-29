27bae36b05ece88604fc0b92e6cfb9f199a8536d
mari <mari.17ymy@gmail.com>
2020-06-28 23:13:37 -0400

    adicionar imagenes como slides con svg

--- lib/pages/slideShow_page.dart
+++ lib/pages/slideShow_page.dart
@@ -6,7 +6,38 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SvgPicture.asset('assets/svg/s1.svg'),
+      body: _Slides()
+      //SvgPicture.asset('assets/svg/s1.svg'),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide(svg: 'assets/svg/s1.svg'),
+          _Slide(svg: 'assets/svg/s2.svg',),
+          _Slide(svg: 'assets/svg/s3.svg',)
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide({this.svg});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: SvgPicture.asset(svg),
     );
   }
 }
