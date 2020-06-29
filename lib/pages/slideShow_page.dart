be4a33a84157933ae1789c2f6dfae024ae69a71d
mari <mari.17ymy@gmail.com>
2020-06-28 22:15:48 -0400

    svgPicture

--- lib/pages/slideShow_page.dart
+++ lib/pages/slideShow_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SlideShowPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('holaaaaaaaa'),),
+      body: SvgPicture.asset('assets/svg/s1'),
     );
   }
 }
