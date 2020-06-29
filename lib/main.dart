065f427d3a92803e93900cde66b08dc62e65eeec
mari <mari.17ymy@gmail.com>
2020-06-28 21:35:57 -0400

    crear slide show page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@
 // import 'package:custom_painter/pages/cuadrado_animado_page.dart';
 // import 'package:custom_painter/pages/header_page.dart';
 // import 'package:custom_painter/pages/circular_progress_page.dart';
-import 'package:custom_painter/pages/graficas_circulares_page.dart';
+// import 'package:custom_painter/pages/graficas_circulares_page.dart';
+import 'package:custom_painter/pages/slideShow_page.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: GraficasCircularesPage()
+      home: SlideShowPage()
     );
   }
 }
