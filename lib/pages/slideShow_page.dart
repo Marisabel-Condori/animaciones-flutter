1e92234d88291efb40f41741ae2487edc439c2c4
mari <mari.17ymy@gmail.com>
2020-06-29 00:18:33 -0400

    puntos del slide

--- lib/pages/slideShow_page.dart
+++ lib/pages/slideShow_page.dart
@@ -6,12 +6,51 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _Slides()
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(child: _Slides()),
+            _Dots()
+          ],
+        ),
+      )
       //SvgPicture.asset('assets/svg/s1.svg'),
     );
   }
 }
+//*****************DOTS********************************//
+class _Dots extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80.0,
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        _Dot(),
+        _Dot(),
+        _Dot()
+      ],),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5.0),
+      height: 10.0, width: 10.0,
+      decoration: BoxDecoration(
+        color: Colors.blue, shape: BoxShape.circle
+      ),);
+  }
+}
+
+//*****************SLIDES********************************//
 class _Slides extends StatelessWidget {
 
   @override
