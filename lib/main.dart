f895800c6717070e8bc23641bbfcf25b8e01add7
mari <mari.17ymy@gmail.com>
2020-07-08 21:33:56 -0400

    inicio de tipo pinterest

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@
 // import 'package:custom_painter/pages/circular_progress_page.dart';
 // import 'package:custom_painter/pages/graficas_circulares_page.dart';
 //import 'package:custom_painter/labs/slideShow_page.dart';
+import 'package:custom_painter/pages/pinterest_page.dart';
 import 'package:custom_painter/pages/slide_show_optimizado_page.dart';
 import 'package:flutter/material.dart';
  
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: SlideShowOptimiPage()
+      home: PinterestPage()
     );
   }
 }
