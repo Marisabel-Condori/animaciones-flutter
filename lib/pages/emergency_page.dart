9cf57d65897521b9362128d641d19bdf9a2f3597
mari <mari.17ymy@gmail.com>
2020-07-10 22:14:38 -0400

    map, streep

--- lib/pages/emergency_page.dart
+++ lib/pages/emergency_page.dart
@@ -4,10 +4,46 @@ import 'package:flutter/material.dart';
 import 'package:custom_painter/widgets/header_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+class ItemBoton {
+
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton( this.icon, this.texto, this.color1, this.color2 );
+}
+
 class EmergencyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final items = <ItemBoton>[
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+    ];
+
+    List<Widget> listaItem = items.map(
+      (item) => BotonMenuWidget(
+        icon: item.icon,
+        onPress: (){print('click');},
+        texto: item.texto,
+        color1: item.color1,
+        color2: item.color2,
+      )
+    ).toList();
+
     return Scaffold(
       body: Stack(
         children: [
@@ -16,7 +52,7 @@ class EmergencyPage extends StatelessWidget {
             child: ListView(
               children: [
                 SizedBox(height: 80.0,),
-                _BotonMenuWidgetTemp(),
+                ...listaItem,
               ]
             ),
           ),
@@ -28,9 +64,6 @@ class EmergencyPage extends StatelessWidget {
 }
 
 class _BotonMenuWidgetTemp extends StatelessWidget {
-  const _BotonMenuWidgetTemp({
-    Key key,
-  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
