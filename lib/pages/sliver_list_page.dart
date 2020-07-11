e83f9ba33544f66be1445d71ecc8fb1d25483f60
mari <mari.17ymy@gmail.com>
2020-07-10 23:40:54 -0400

    items personalizados

--- lib/pages/sliver_list_page.dart
+++ lib/pages/sliver_list_page.dart
@@ -4,8 +4,9 @@ class SliverListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: _Titulo()
+      body: _ItemListView()
       //_ItemListView(),
     );
   }
@@ -41,25 +42,47 @@ class _Titulo extends StatelessWidget {
 
 class _ItemListView extends StatelessWidget {
 
+  final items = [
+      _ItemPersonalizado( 'Orange', Color(0xffF08F66) ),
+      _ItemPersonalizado( 'Family', Color(0xffF2A38A) ),
+      _ItemPersonalizado( 'Orange', Color(0xffF08F66) ),
+      _ItemPersonalizado( 'Family', Color(0xffF2A38A) ),
+      _ItemPersonalizado( 'Orange', Color(0xffF08F66) ),
+      _ItemPersonalizado( 'Family', Color(0xffF2A38A) ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return Container(
-          child: Text('green', style: TextStyle(color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.bold),),
-          margin: EdgeInsets.all(10.0),
-          padding: EdgeInsets.all(10.0),
-          alignment: Alignment.centerLeft,
-          height: 100.0,
-          width: 100.0,
-          decoration: BoxDecoration(
-            color: Colors.green,
-            borderRadius: BorderRadius.circular(10.0)
-          ),
-        );
-      },
+      itemCount: items.length,
+      itemBuilder: (context, index) => items[index]
+      
 
+    );
+  }
+}
+
+class _ItemPersonalizado extends StatelessWidget {
+  final String texto;
+  final Color color;
+
+  _ItemPersonalizado(
+    this.texto, this.color
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(this.texto, style: TextStyle(color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.bold),),
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(10.0),
+      alignment: Alignment.centerLeft,
+      height: 100.0,
+      width: 100.0,
+      decoration: BoxDecoration(
+        color: this.color,
+        borderRadius: BorderRadius.circular(10.0)
+      ),
     );
   }
 }
