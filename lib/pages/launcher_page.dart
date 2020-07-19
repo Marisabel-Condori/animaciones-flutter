a810abbb3f972f6e18350754f056b7222bd82911
mari <mari.17ymy@gmail.com>
2020-07-19 01:33:50 -0400

    color accent

--- lib/pages/launcher_page.dart
+++ lib/pages/launcher_page.dart
@@ -21,19 +21,20 @@ class _ListaOpciones extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appTheme = Provider.of<ThemeChanger>(context).currentTheme;
     return ListView.separated(
       physics: BouncingScrollPhysics(),
       itemCount: pageRoutes.length,
       itemBuilder: (context, i) => ListTile(
-        leading: FaIcon(pageRoutes[i].icon, color: Colors.blue,),
+        leading: FaIcon(pageRoutes[i].icon, color: appTheme.accentColor,),
         title: Text(pageRoutes[i].titulo),
-        trailing: Icon(Icons.chevron_right, color: Colors.blue,),
+        trailing: Icon(Icons.chevron_right, color: appTheme.accentColor),
         onTap: (){
           Navigator.push(context, MaterialPageRoute(builder: (context)=>pageRoutes[i].page));
         },
       ), 
       separatorBuilder: (context, i) => Divider(
-        color: Colors.blue
+        color: appTheme.primaryColorLight
         
       ), 
     );
@@ -45,6 +46,7 @@ class _Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appTheme = Provider.of<ThemeChanger>(context);
+    final acentColor = appTheme.currentTheme.accentColor;
     return Drawer(
       child: Container(
         child: Column(
@@ -54,12 +56,13 @@ class _Menu extends StatelessWidget {
                 padding: EdgeInsets.all(10.0),
                 width: double.infinity,
                 height: 200.0,
-                child: CircleAvatar(child: Text('MA', style: TextStyle(fontSize: 80.0),) )
+                child: CircleAvatar(child: Text('MA', style: TextStyle(fontSize: 80.0),),
+                backgroundColor: acentColor )
               ),
             ),
             Expanded(child: _ListaOpciones()),
             ListTile(
-              leading: Icon(Icons.lightbulb_outline, color: Colors.blue,),
+              leading: Icon(Icons.lightbulb_outline, color: acentColor,),
               title: Text('Dark Mode'),
               trailing: Switch.adaptive(
                 value: appTheme.getDarkTheme, 
@@ -70,7 +73,7 @@ class _Menu extends StatelessWidget {
               bottom: true,
               top: false, left: false, right: false,
               child: ListTile(
-                leading: Icon(Icons.add_to_home_screen, color: Colors.blue,),
+                leading: Icon(Icons.add_to_home_screen, color: acentColor,),
                 title: Text('Custom Theme'),
                 trailing: Switch.adaptive(
                   value: appTheme.getCustomTheme, 
